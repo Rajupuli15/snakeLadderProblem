033a54ded5bb8e298936bfeaa3170dc90df6f06b
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-08 08:08:43 -0400

    Added:ladderOrSnake

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,29 @@
 package com.company;
 
 public class Main {
+    static int noTurn = 0;
+    static int ladder = 1;
+    static int snake = 2;
+    static int Sum = 0;
 
     public static void main(String[] args) {
         int dice = (int) (Math.random() * 6) + 1;
-        System.out.println(dice);
+
+        System.out.println("Dice Roll is " + dice);
+
+        int opt = (int) Math.floor(Math.random() *10) % 3;
+
+        if (opt == ladder) {
+            Sum = Sum + dice;
+            System.out.println("Ladder");
+        }
+        else if (opt == snake) {
+            Sum = Sum - dice;
+            System.out.println("Snake");
+        }
+        else {
+            System.out.println("NoTurn");
+        }
 
     }
 }
