c11706ceba57ba04e31d985db6f24671e6038c73
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-08 07:43:17 -0400

    Added:PositionZero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Welcome to snakeLadder");
+	System.out.println("Player at Start Position Zero");
+
     }
 }
